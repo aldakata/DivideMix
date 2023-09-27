7c94faa82d3082f45de3fe827341d530afa1a9f2
aldakata <ac.tatjer@gmail.com>
2023-09-27 13:19:55 +0200

    noise labels and clean labels to dataloader

--- dataloader_cifar.py
+++ dataloader_cifar.py
@@ -116,7 +116,6 @@ class cifar_dataset(Dataset):
         probability=[],
         log="",
     ):
-
         self.r = r  # noise ratio
         self.transform = transform
         self.mode = mode
@@ -167,7 +166,10 @@ class cifar_dataset(Dataset):
             train_data = train_data.transpose((0, 2, 3, 1))
 
             if os.path.exists(noise_file):
-                noise_label = json.load(open(noise_file, "r"))
+                # noise_label = json.load(open(noise_file, "r"))
+                noise_file = np.load(noise_file, allow_pickle=True)
+                train_label = noise_file.item().get("clean_label")
+                noise_label = noise_file.item().get("noise_label")
             else:  # inject noise
                 noise_label = []
                 idx = list(range(50000))
