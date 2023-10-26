ebe6ee6b2613f5bb2d6cdfb5233f35073ff0aa1f
aldakata <ac.tatjer@gmail.com>
2023-10-26 13:34:21 +0200

    ready to resume once

--- processing_utils.py
+++ processing_utils.py
@@ -7,11 +7,11 @@ def load_net_optimizer_from_ckpt_to_device(net, args, ckpt_path, device):
     print(f"[ LOADING NET] {ckpt_path}\n")
     ckpt = torch.load(ckpt_path, map_location="cpu")
     device = torch.device(device)
-    net.load_state_dict(ckpt["model_state_dict"])
+    net.load_state_dict(ckpt["net"])
     net.to(device)
 
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    optimizer.load_state_dict(ckpt["optimizer_state_dict"])
+    optimizer.load_state_dict(ckpt["Optimizer"])
 
     return net, optimizer
 
